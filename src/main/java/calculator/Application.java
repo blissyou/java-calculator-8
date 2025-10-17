cb372ae044a64c0094318da2dd77b5b329242065
원형 <147372294+blissyou@users.noreply.github.com>
2025-10-17 15:55:51 +0900

    feat: 문자열 -> 배열 교체 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,47 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.*;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+
+        String[] inputArray = input.split("");
+
+        Integer result = 0;
+
+        String operator;
+
+
+        if(inputArray[0].equals("/") && inputArray[1].equals("/")
+                && inputArray[3].equals("\\")&& inputArray[4].equals("n")){
+            operator = inputArray[2];
+            System.arraycopy(inputArray, 5, inputArray, 0, inputArray.length - 5);
+            inputArray = input.split("["+operator+",:]");
+
+            for(var o = 0; o <inputArray.length; o++){
+               System.out.println(inputArray[o]);
+               result += stringToInteger(inputArray[o]);
+
+            }
+        }else{
+            for(var o = 0; o <inputArray.length; o++){
+                inputArray = input.split("[,:]");
+                result += stringToInteger(inputArray[o]);
+            }
+        }
+
+        System.out.println("결과 : "+result);
+    }
+
+    public static Integer stringToInteger(String input){
+        try{
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
     }
 }
